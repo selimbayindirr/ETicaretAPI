d1560f01a8e5bbce3a142e4954efa50d2f5e90db
selimbayindir28@gmail.com <selimbayindir28@gmail.com>
2023-06-19 21:34:07 +0300

    Repo

---
+++ Core/ETicaretAPI.Application/Repositories/IWriteRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Repositories
+{
+    public interface IWriteRepository<T> : IRepository<T> where T : class
+    {
+        //insert update delete
+
+        Task<bool> AddAsync(T entity);
+        Task<bool> AddAsync(List<T> entity);
+        Task<bool> Remove(T entity);
+        Task<bool> Remove(Guid id);
+        Task<bool> UpdateAsync(T entity);
+    }
+}
---
+++ infrastructure/ETicaret.Persistence/Concretes/Repositories/ReadRepository.cs
@@ -0,0 +1,38 @@
+﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Concretes.Repositories
+{
+    public class ReadRepository<T> : IReadRepository<T> where T : class
+    {
+        private readonly ETicaretAPIDbContext _context;
+
+        public ReadRepository(ETicaretAPIDbContext context)
+        {
+            _context = context;
+        }
+
+        public DbSet<T> Table => _context.Set<T>();
+
+        public IQueryable<T> GetAll()=>  Table;
+        
+        public async Task<T> GetSingleAsync(Expression<Func<T, bool>> expression)
+            =>await  Table.FirstOrDefaultAsync(expression);
+
+        public IQueryable<T> GetWhere(Expression<Func<T, bool>> expression)
+                =>Table.Where(expression);
+        public Task<T> GETByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+            //Table.FirstOrDefaultAsync<T>().Id = id;
+        }
+
+    }
+}
